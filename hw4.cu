491522b2deb80a6152420516ca59cf848662de4b
Miller <hidden@guess.com>
2025-04-23 12:47:44 +0800

    modify hw4

--- hw4.cu
+++ hw4.cu
@@ -1,4 +1,3 @@
-#include <cuda_device_runtime_api.h>
 #include <cuda_runtime.h>
 #include <stdio.h>
 
@@ -17,12 +16,18 @@ int main() {
 
   float *hostPtr;
   float *devicePtr;
-
+  float *data;
   // destribute host memory
   int N = 2 * global_mem / sizeof(float);
-  cudaHostAlloc((void **)&hostPtr, N * sizeof(float), cudaHostAllocDefault);
+  cudaMallocManaged((void **)&data, N * sizeof(float));
+  // cudaHostAlloc((void **)&hostPtr, N * sizeof(float), cudaHostAllocDefault);
   for (int i = 0; i < 2 * global_mem / sizeof(float); i += 1) {
     hostPtr[i] = i;
+  }
+
+  cudaError_t err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    printf("cudaHostAlloc failed: %s\n", cudaGetErrorString(err));
   }
 
   for (int i = 0; i < 10; i += 1) {
@@ -31,11 +36,12 @@ int main() {
   printf("\n");
 
   // get map hostPtr to devicePtr
-  cudaHostGetDevicePointer((void **)&devicePtr, (void **)&hostPtr, 0);
+  // cudaHostGetDevicePointer((void **)&devicePtr, (void **)&hostPtr, 0);
 
   // call kernel "twice"
   int grid_size = (N + 1024 - 1) / 1024;
-  twice<<<grid_size, 1024>>>(devicePtr);
+  // twice<<<grid_size, 1024>>>(devicePtr);
+  twice<<<grid_size, 1024>>>(data);
   cudaDeviceSynchronize();
 
   cudaError_t error = cudaGetLastError();
